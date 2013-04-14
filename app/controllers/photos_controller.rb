06d06da56b95f906bd1ec71a838aa3657227020b
Nicolas Block <nick@headlineshirts.net>
2013-04-13 18:46:10 -0700

    allow virtual attributes to be sent in the to_json method

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,14 @@
 class PhotosController < ApplicationController
+  
+  respond_to :json
+  
+  
   def index
+
   end
 
   def create
+    @photo = Photo.create(params[:photo])
+    respond_with(@photo, :methods => ['image_path'], :only => [:x_pos, :y_pos, :z_pos, :scale])
   end
 end
