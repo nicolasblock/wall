55e72f29f68a369f68cf73e11d75bebcfc6a23e3
Nicolas Block <nick@headlineshirts.net>
2013-04-13 18:45:18 -0700

    adds logic for attributes and obfuscating image file names

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,14 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :scale, :x_pos, :y_pos, :z_pos
+
+  attr_accessible :scale, :x_pos, :y_pos, :z_pos, :image #nb
+
+  # Paperclip attachment
+  has_attached_file :image,
+      :url => '/system/images/:hash.:extension',
+      :hash_secret => 'doremifasolatido' #nb
+      
+  def image_path
+    ActionController::Base.helpers.asset_path(image.url) #nb
+    #"/public/system/images/#{image_file_name}"
+  end
 end
